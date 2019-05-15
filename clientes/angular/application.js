44c47318ac190ac3ec968dd2807c4d301c244b36
Lucian Ribas <ribas.lucian@gmail.com>
2019-05-15 18:20:43 -0300

    up

--- clientes/angular/application.js
+++ clientes/angular/application.js
@@ -1,25 +1,111 @@
 var app = angular.module('App', []);
 
-app.controller('HotelController', function ($scope, $http) {
-
-
-
-    $scope.register = function () {
-
-        console.log($scope.package);
-
-    }
+app.controller('PackagesController', function ($scope, $http) {
     
-
-    $scope.list = function () {
-
+    // client methods
+    
+    $scope.packages_list = function () {
         $http({
             method: 'GET',
-            url: 'localhost/packages'
-        }).then(function successCallback(response) {
-            console.log(response);
+            url: 'http://localhost:8080/packages'
+        }).then(function (response) {
+            $scope.clients_packages = response.data.packages;
         });
-
     }
 
+
+    $scope.package_register = function () {
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8080/packages',
+            data: $scope.package,
+        }).then(function (response) {
+            $scope.packages_list();
+        });
+    }
+
+
+    $scope.package_cancel = function () {
+        $http({
+            method: 'DELETE',
+            url: 'http://localhost:8080/packages',
+            data: $scope.package,
+        }).then(function (response) {
+            $scope.packages_list();
+        });
+    }
+
+
+    $scope.package_pay = function () {
+        $http({
+            method: 'PUT',
+            url: 'http://localhost:8080/packages',
+            data: $scope.package,
+        }).then(function (response) {
+            $scope.packages_list();
+        });
+    }
+
+
+    // server methods
+
+    $scope.flights_list = function () {
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8080/flights'
+        }).then(function (response) {
+            $scope.flights = response.data.flights;
+        });
+    }
+
+    $scope.flight_register = function () {
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8080/flights',
+            data: $scope.flight,
+        }).then(function (response) {
+            $scope.flights_list();
+        });
+    }
+
+    $scope.flight_cancel = function () {
+        $http({
+            method: 'DELETE',
+            url: 'http://localhost:8080/flights',
+            data: $scope.flight,
+        }).then(function (response) {
+            $scope.flights_list();
+        });
+    }
+    
+    
+
+    $scope.hosts_list = function () {
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8080/hosts'
+        }).then(function (response) {
+            $scope.hosts = response.data.hosts;
+        });
+    }
+
+    $scope.host_register = function () {
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8080/hosts',
+            data: $scope.host,
+        }).then(function (response) {
+            $scope.hosts_list();
+        });
+    }
+    
+    $scope.host_cancel = function () {
+        $http({
+            method: 'DELETE',
+            url: 'http://localhost:8080/hosts',
+            data: $scope.host,
+        }).then(function (response) {
+            $scope.hosts_list();
+        });
+    }
 });
